07eff06fd2fd7f13f59949ff02c8546324acc485
Tuomo <tuokri@tuta.io>
2022-12-18 20:05:06 +0200

    Add Redis caching

--- app.py
+++ app.py
@@ -3,19 +3,21 @@ import ctypes
 import multiprocessing as mp
 import os
 import random
+import shlex
 import ssl
 import urllib.parse
+from base64 import b64decode
+from base64 import b64encode
 from contextlib import asynccontextmanager
 from types import SimpleNamespace
-from types import TracebackType
 from typing import AsyncIterator
 from typing import Optional
-from typing import Type
 from urllib.parse import urlparse
 from urllib.parse import urlunparse
 
 import aiohttp
 import certifi
+import orjson
 import redis.asyncio as redis
 from dotenv import load_dotenv
 from multidict import CIMultiDictProxy
@@ -39,9 +41,21 @@ class AppContext(SimpleNamespace):
         self._vercel_url_components: urllib.parse.ParseResult = urlparse(self._vercel_url)
         self._aiohttp_session: Optional[aiohttp.ClientSession] = None
         self._redis: Optional[redis.Redis] = None
+        self._vercel_session: Optional[VercelSession] = None
 
         self.ssl_context = ssl.create_default_context(cafile=certifi.where())
 
+    @property
+    def vercel_session(self) -> "VercelSession":
+        if not self._vercel_session:
+            raise RuntimeError("vercel_session not initialized")
+        return self._vercel_session
+
+    @vercel_session.setter
+    def vercel_session(self, vercel_session: "VercelSession"):
+        self._vercel_session = vercel_session
+
+    # TODO: refactor this into VercelSession.
     @property
     def vercel_url(self) -> str:
         return self._vercel_url
@@ -88,7 +102,8 @@ app = CustomSanic(
 )
 
 
-async def refresh_cache(_app: CustomSanic):
+async def refresh_cache():
+    """TODO: this isn't actually needed it seems..."""
     refreshing = False
 
     # Should probably wait her for all workers to initialize
@@ -102,28 +117,43 @@ async def refresh_cache(_app: CustomSanic):
     while True:
         # print("hi from", os.getpid())
 
-        with _app.shared_ctx.cache_refresh_needed.get_lock():
+        with app.shared_ctx.cache_refresh_needed.get_lock():
             # print(_app.shared_ctx.cache_refresh_needed.value)
-            if _app.shared_ctx.cache_refresh_needed.value:
-                _app.shared_ctx.cache_refresh_needed.value = False
+            if app.shared_ctx.cache_refresh_needed.value:
+                app.shared_ctx.cache_refresh_needed.value = False
                 refreshing = True
 
         if refreshing:
-            # print("doing refresh in", os.getpid(), "*" * 80, time.time())
-            await asyncio.sleep(5)
-            with _app.shared_ctx.cache_refresh_needed.get_lock():
-                _app.shared_ctx.cache_refresh_needed.value = True
+            await asyncio.sleep(15)
+            with app.shared_ctx.cache_refresh_needed.get_lock():
+                app.shared_ctx.cache_refresh_needed.value = True
                 refreshing = False
 
         await asyncio.sleep(2)
 
 
-app.add_task(refresh_cache)
+# app.add_task(refresh_cache())
+
+
+def parse_kv_pairs(s: str) -> dict:
+    lexer = shlex.shlex(s.replace(" ", ""), posix=True)
+    lexer.whitespace = ","
+    lexer.wordchars += "=-"
+    # noinspection PyTypeChecker
+    return dict(word.split("=", maxsplit=1) for word in lexer)
 
 
 class VercelSession:
-    def __init__(self, request: Request):
+    def __init__(self) -> None:
         self._resp: Optional[aiohttp.ClientResponse] = None
+        self._vercel_route = ""
+        # TODO: turn this into a dataclass or separate instance variables.
+        self._cached_data: dict[
+            str,
+            CIMultiDictProxy[str] | bytes | str] = {}
+
+    @asynccontextmanager
+    async def get(self, request: Request) -> AsyncIterator[CIMultiDictProxy]:
         self._vercel_route = urlunparse((
             "",
             "",
@@ -133,47 +163,80 @@ class VercelSession:
             None,
         ))
 
-    async def __aenter__(self):
-        return self
-
-    async def __aexit__(
-            self,
-            __exc_type: Optional[Type[BaseException]],
-            __exc_value: Optional[BaseException],
-            __traceback: Optional[TracebackType],
-    ):
-        return None
-
-    @asynccontextmanager
-    async def get(self) -> AsyncIterator[CIMultiDictProxy]:
-        if app.ctx.redis:
-            pass
+        if await app.ctx.redis.exists(self._vercel_route):
+            headers = self._cached_data.get("headers")
+            if headers:
+                yield headers
+                return
 
         async with app.ctx.aiohttp_session.get(
-                self._vercel_route, ssl_context=app.ctx.ssl_context) as resp:
+                url=self._vercel_route, ssl_context=app.ctx.ssl_context) as resp:
             self._resp = resp
             yield self._resp.headers
 
     async def iter_chunked(self, n: int = 4096) -> AsyncIterator[bytes]:
         if not self._resp:
-            raise RuntimeError("cannot begin streaming before get is performed")
-        # TODO: cache response+bytes with Redis here?
+            raise RuntimeError("cannot iterate chunks before get is performed")
+
+        vr = self._vercel_route
+        if await app.ctx.redis.exists(vr):
+            payload_bytes = b""
+            redis_key = self._cached_data.get("redis_key")
+            if redis_key != vr:
+                cached = await app.ctx.redis.get(vr)
+                if cached:
+                    cached_dict = orjson.loads(cached)
+                    payload = cached_dict.get("payload", "")
+                    if payload:
+                        payload_bytes = b64decode(payload)
+                        self._cached_data = {
+                            "redis_key": vr,
+                            "headers": self._resp.headers,
+                            "payload": payload_bytes,
+                        }
+            else:
+                payload_bytes = self._cached_data.get("payload", b"")
+
+            if payload_bytes:
+                yield payload_bytes
+                return
+
+        chunks = b""
         async for chunk in self._resp.content.iter_chunked(n):
+            chunks += chunk
             yield chunk
+
+        await app.ctx.redis.set(vr, orjson.dumps({
+            "headers": {str(k): str(v) for k, v in self._resp.headers.items()},
+            "payload": b64encode(chunks).decode("utf-8"),
+        }))
+        self._cached_data = {
+            "redis_key": vr,
+            "headers": self._resp.headers,
+            "payload": chunks,
+        }
+
+        cc = self._resp.headers.get("cache-control")
+        if cc:
+            kvs = parse_kv_pairs(cc)
+            try:
+                ttl = int(kvs.get("max-age", 1000))
+                await app.ctx.redis.expire(vr, ttl)
+            except ValueError:
+                pass
 
 
 async def vercel_get(request: Request):
-    async with VercelSession(request) as vs:
-        async with vs.get() as vercel_headers:
-            vercel_headers: CIMultiDictProxy  # type: ignore[no-redef]
-            response = await request.respond(
-                headers={
-                    "Content-Encoding": vercel_headers.get("Content-Encoding"),
-                },
-                content_type=vercel_headers.get("Content-Type"),
-            )
-            async for data in vs.iter_chunked():
-                await response.send(data)
+    async with app.ctx.vercel_session.get(request) as vercel_headers:
+        vercel_headers: CIMultiDictProxy  # type: ignore[no-redef]
+        response = await request.respond(
+            headers={
+                "content-encoding": vercel_headers.get("content-encoding", ""),
+            },
+            content_type=vercel_headers.get("content-type"),
+        )
+        async for data in app.ctx.vercel_session.iter_chunked():
+            await response.send(data)
 
 
 @app.before_server_start
@@ -183,7 +246,9 @@ async def before_server_start(_app: CustomSanic):
         auto_decompress=False,
     )
 
-    _app.ctx.redis = redis.Redis(
+    _app.ctx.vercel_session = VercelSession()
+
+    _app.ctx.redis = redis.StrictRedis(
         connection_pool=REDIS_POOL,
     )
     try:
@@ -199,6 +264,7 @@ async def before_server_start(_app: CustomSanic):
 @app.after_server_stop
 async def after_server_stop(_app: CustomSanic):
     await _app.ctx.aiohttp_session.close()
+    await _app.ctx.redis.close()
 
 
 @app.main_process_start
