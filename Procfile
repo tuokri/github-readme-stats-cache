71d18e094f88c01f9b8329752007776db46088e6
Tuomo <tuokri@tuta.io>
2022-12-27 21:21:50 +0200

    Implement proper caching and background tasks

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
-web: sanic app.app --host=0.0.0.0 --port=8080 --workers=4
+web: sanic app.app --host=0.0.0.0 --port=8080 --workers=3 --access-log
+celery_worker: celery -A worker:app worker --loglevel=info --concurrency=1
---
+++ worker.py
@@ -0,0 +1,80 @@
+import os
+from base64 import b64encode
+from traceback import print_exc
+from urllib.parse import urlparse
+from urllib.parse import urlunparse
+
+import celery
+import orjson
+import redis
+import requests
+from dotenv import load_dotenv
+
+from cache import CACHE_LOCK
+from cache import DISK_CACHE
+from utils import parse_kv_pairs
+
+load_dotenv()
+
+app = celery.Celery("tasks", broker=os.environ["REDIS_URL"])
+redis = redis.StrictRedis(os.environ["REDIS_URL"])
+cache = DISK_CACHE
+cache_lock = CACHE_LOCK
+
+
+def set_redis(key: str, value: bytes, ttl: int):
+    # noinspection PyBroadException
+    try:
+        redis.set(key, value)
+        redis.expire(key, ttl)
+    except Exception:
+        print_exc()
+
+
+def set_cache(key: str, value: bytes):
+    # noinspection PyBroadException
+    try:
+        with cache_lock:
+            cache.set(key, value)
+    except Exception:
+        print_exc()
+
+
+@app.task
+def do_vercel_get(vercel_url: str, vercel_route: str):
+    vercel_url_parts = urlparse(vercel_url)
+    vercel_route_parts = urlparse(vercel_route)
+    # scheme, netloc, url, params, query, fragment
+    url = urlunparse((
+        vercel_url_parts.scheme,
+        vercel_url_parts.netloc,
+        vercel_route_parts.path,
+        None,
+        vercel_route_parts.query,
+        None,
+    ))
+
+    resp = requests.get(url, timeout=30, stream=True)
+    headers = resp.headers
+    data = b""
+    for chunk in resp.iter_content():
+        data += chunk
+
+    dump_headers = {str(k): str(v) for k, v in headers.items()}
+    dump_payload = b64encode(data).decode("utf-8")
+    json_dump = orjson.dumps({
+        "headers": dump_headers,
+        "payload": dump_payload,
+    })
+
+    set_cache(url, json_dump)
+
+    ttl = 7500
+    cc = headers.get("cache-control")
+    if cc:
+        kvs = parse_kv_pairs(cc)
+        try:
+            ttl = int(kvs.get("max-age", 7500))
+        except ValueError:
+            pass
+    set_redis(url, json_dump, ttl)
